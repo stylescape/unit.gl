eb307aaa3bf2303a35cf8e48e0ac8751e9407424
Lars van Vianen <lars@vianen.com>
2024-08-18 06:22:46 +0200

    Create BoxModel.ts

---
+++ src/ts/BoxModel.ts
@@ -0,0 +1,40 @@
+class BoxModel {
+    margin: Margin;
+    padding: Padding;
+    border: Border;
+    size: Size;
+
+    constructor(margin: Margin, padding: Padding, border: Border, size: Size) {
+        if (
+            margin.top.unit !== size.width.unit ||
+            padding.top.unit !== size.width.unit ||
+            border.width.unit !== size.width.unit
+        ) {
+            throw new Error('All units in BoxModel must match');
+        }
+        this.margin = margin;
+        this.padding = padding;
+        this.border = border;
+        this.size = size;
+    }
+
+    setMargin(margin: Margin): void {
+        this.margin = margin;
+    }
+
+    setPadding(padding: Padding): void {
+        this.padding = padding;
+    }
+
+    setBorder(border: Border): void {
+        this.border = border;
+    }
+
+    setSize(size: Size): void {
+        this.size = size;
+    }
+
+    toString(): string {
+        return `BoxModel:\n  Size: ${this.size.toString()}\n  Margin: ${this.margin.toString()}\n  Padding: ${this.padding.toString()}\n  Border: ${this.border.toString()}`;
+    }
+}
