824ac2eaeca494732c08028596d2c1b4e45ef879
Lars van Vianen <lars@vianen.com>
2024-08-18 06:21:07 +0200

    Create Size.ts

---
+++ src/ts/Size.ts
@@ -0,0 +1,32 @@
+class Size {
+    width: Unit;
+    height: Unit;
+
+    constructor(width: Unit, height: Unit) {
+        if (width.unit !== height.unit) {
+            throw new Error('Width and height must have the same unit');
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    add(other: Size): Size {
+        return new Size(this.width.add(other.width), this.height.add(other.height));
+    }
+
+    subtract(other: Size): Size {
+        return new Size(this.width.subtract(other.width), this.height.subtract(other.height));
+    }
+
+    scale(factor: number): Size {
+        return new Size(this.width.multiply(factor), this.height.multiply(factor));
+    }
+
+    convert(toUnit: UnitType): Size {
+        return new Size(this.width.convert(toUnit), this.height.convert(toUnit));
+    }
+
+    toString(): string {
+        return `Size: ${this.width.toString()} x ${this.height.toString()}`;
+    }
+}
