1931f211b134c0d739c3e37d669811aa1a8290e5
Lars van Vianen <lars@vianen.com>
2024-08-18 06:23:44 +0200

    Create Spacing.ts

---
+++ src/ts/Spacing.ts
@@ -0,0 +1,68 @@
+class Spacing {
+    top: Unit;
+    right: Unit;
+    bottom: Unit;
+    left: Unit;
+
+    constructor(top: Unit, right: Unit, bottom: Unit, left: Unit) {
+        if (top.unit !== right.unit || top.unit !== bottom.unit || top.unit !== left.unit) {
+            throw new Error('All sides must have the same unit');
+        }
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+    }
+
+    add(other: Spacing): Spacing {
+        return new Spacing(
+            this.top.add(other.top),
+            this.right.add(other.right),
+            this.bottom.add(other.bottom),
+            this.left.add(other.left)
+        );
+    }
+
+    subtract(other: Spacing): Spacing {
+        return new Spacing(
+            this.top.subtract(other.top),
+            this.right.subtract(other.right),
+            this.bottom.subtract(other.bottom),
+            this.left.subtract(other.left)
+        );
+    }
+
+    scale(factor: number): Spacing {
+        return new Spacing(
+            this.top.multiply(factor),
+            this.right.multiply(factor),
+            this.bottom.multiply(factor),
+            this.left.multiply(factor)
+        );
+    }
+
+    convert(toUnit: UnitType): Spacing {
+        return new Spacing(
+            this.top.convert(toUnit),
+            this.right.convert(toUnit),
+            this.bottom.convert(toUnit),
+            this.left.convert(toUnit)
+        );
+    }
+
+    toString(): string {
+        return `Spacing: ${this.top.toString()} ${this.right.toString()} ${this.bottom.toString()} ${this.left.toString()}`;
+    }
+}
+
+class Margin extends Spacing {
+    constructor(top: Unit, right: Unit, bottom: Unit, left: Unit) {
+        super(top, right, bottom, left);
+    }
+}
+
+class Padding extends Spacing {
+    constructor(top: Unit, right: Unit, bottom: Unit, left: Unit) {
+        super(top, right, bottom, left);
+    }
+}
