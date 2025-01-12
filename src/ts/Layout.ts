a24a6773f3e2d6500ae34a3375e1f039997c29ee
Lars van Vianen <lars@vianen.com>
2025-01-12 17:02:42 +0100

    Update Layout.ts

--- src/ts/Layout.ts
+++ src/ts/Layout.ts
@@ -1,20 +1,50 @@
+// ============================================================================
+// Import
+// ============================================================================
+
+import Unit from "./Unit";
+
+
+// ============================================================================
+// Types
+// ============================================================================
+
 type Align = 'start' | 'center' | 'end' | 'stretch';
 type Justify = 'start' | 'center' | 'end' | 'space-between' | 'space-around';
 
-class Layout {
+// ============================================================================
+// Classes
+// ============================================================================
+
+
+
+export default class Layout {
+
+    // Parameters
+    // ========================================================================
+
     width: Unit;
     height: Unit;
     align: Align;
     justify: Justify;
     direction: 'row' | 'column';
 
-    constructor(width: Unit, height: Unit, align: Align = 'stretch', justify: Justify = 'start', direction: 'row' | 'column' = 'row') {
+    // Constructor
+    // ========================================================================
+
+    constructor(
+        width: Unit,
+        height: Unit,
+        align: Align = 'stretch', justify: Justify = 'start', direction: 'row' | 'column' = 'row') {
         this.width = width;
         this.height = height;
         this.align = align;
         this.justify = justify;
         this.direction = direction;
     }
+
+    // Methods
+    // ========================================================================
 
     setAlign(align: Align): void {
         this.align = align;
