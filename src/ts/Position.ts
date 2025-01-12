4597733387b4379461989d9768341b14787d021c
Lars van Vianen <lars@vianen.com>
2025-01-12 17:02:20 +0100

    Update Position.ts

--- src/ts/Position.ts
+++ src/ts/Position.ts
@@ -1,14 +1,38 @@
-class Position {
+// ============================================================================
+// Import
+// ============================================================================
+
+import Unit from "./Unit";
+
+
+// ============================================================================
+// Classes
+// ============================================================================
+
+export default class Position {
+
+    // Parameters
+    // ========================================================================
+
     x: Unit;
     y: Unit;
 
-    constructor(x: Unit, y: Unit) {
+    // Constructor
+    // ========================================================================
+
+    constructor(
+        x: Unit,
+        y: Unit
+    ) {
         if (x.unit !== y.unit) {
             throw new Error('X and Y must have the same unit');
         }
         this.x = x;
         this.y = y;
     }
+
+    // Methods
+    // ========================================================================
 
     add(other: Position): Position {
         return new Position(this.x.add(other.x), this.y.add(other.y));
