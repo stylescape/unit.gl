d80947ea66bb79db44ecf172b5b01589f8e78a19
Lars van Vianen <lars@vianen.com>
2025-01-12 17:02:35 +0100

    Update ResponsiveScale.ts

--- src/ts/ResponsiveScale.ts
+++ src/ts/ResponsiveScale.ts
@@ -1,13 +1,36 @@
-class ResponsiveScale {
+// ============================================================================
+// Import
+// ============================================================================
+
+import Unit from "./Unit";
+import Viewport from "./Viewport";
+
+
+// ============================================================================
+// Classes
+// ============================================================================
+
+export default class ResponsiveScale {
+
+    // Parameters
+    // ========================================================================
+
     viewport: Viewport;
     baseSize: Unit;
     scaleFactor: number;
+
+    // Constructor
+    // ========================================================================
 
     constructor(viewport: Viewport, baseSize: Unit, scaleFactor: number) {
         this.viewport = viewport;
         this.baseSize = baseSize;
         this.scaleFactor = scaleFactor;
     }
+
+
+    // Methods
+    // ========================================================================
 
     calculateResponsiveSize(): Unit {
         const ratio = this.viewport.width.value / 1920; // Assuming 1920px is the base
