90dedf27c18654e9bf831fbd455a85be34fb25a1
Lars van Vianen <lars@vianen.com>
2025-05-10 11:08:37 +0200

    Formatting ts

--- src/ts/Size.ts
+++ src/ts/Size.ts
@@ -22,7 +22,9 @@ export default class Size {
 
     constructor(width: Unit, height: Unit) {
         if (width.unit !== height.unit) {
-            throw new Error("Width and height must have the same unit");
+            throw new Error(
+                "Width and height must have the same unit"
+            );
         }
         this.width = width;
         this.height = height;
@@ -32,19 +34,31 @@ export default class Size {
     // ========================================================================
 
     add(other: Size): Size {
-        return new Size(this.width.add(other.width), this.height.add(other.height));
+        return new Size(
+            this.width.add(other.width),
+            this.height.add(other.height)
+        );
     }
 
     subtract(other: Size): Size {
-        return new Size(this.width.subtract(other.width), this.height.subtract(other.height));
+        return new Size(
+            this.width.subtract(other.width),
+            this.height.subtract(other.height)
+        );
     }
 
     scale(factor: number): Size {
-        return new Size(this.width.multiply(factor), this.height.multiply(factor));
+        return new Size(
+            this.width.multiply(factor),
+            this.height.multiply(factor)
+        );
     }
 
     convert(toUnit: UnitType): Size {
-        return new Size(this.width.convert(toUnit), this.height.convert(toUnit));
+        return new Size(
+            this.width.convert(toUnit),
+            this.height.convert(toUnit)
+        );
     }
 
     toString(): string {
